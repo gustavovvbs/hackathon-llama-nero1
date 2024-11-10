5bfc08b5e2d245cc178b59bbcff4c26f411dd2c7
Samuel Jabes <samueljcc@al.insper.edu.br>
2024-11-09 21:37:24 -0300

    feat: change dockerfile and llm to groq

--- utils.py
+++ utils.py
@@ -7,11 +7,13 @@ from llama_parse import LlamaParse
 import httpx 
 from io import BytesIO 
 import tempfile 
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
 import os
 
 load_dotenv()
 
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 async def process_pdf(pdf_url):
     async def download_pdf(url):
@@ -40,38 +42,43 @@ async def process_pdf(pdf_url):
 
     parsing_result = await parser.aload_data(file_path)
 
-    llm = ChatOpenAI(
-        model='gpt-4o-2024-08-06',
-        temperature=0
+    # llm = ChatOpenAI(
+    #     model='gpt-4o-2024-08-06',
+    #     temperature=0
+    # )
+    
+    llm = ChatGroq(
+        api_key=GROQ_API_KEY,
+        model="llama3-70b-8192"
     )
 
     class Transaction(BaseModel):
         """Extrai valores relevantes de um documento de extrato bancário de um usuário"""
         tipo: str = Field(description="Classificação da transferência", examples=["""Alimentação: Transações relacionadas à aquisição de alimentos e bebidas, seja para consumo doméstico ou em estabelecimentos comerciais, como supermercados, restaurantes, lanchonetes e serviços de entrega de refeições.
 
-Transporte: Despesas associadas ao deslocamento, incluindo abastecimento de veículos, uso de transporte público, serviços de transporte por aplicativo, manutenção de automóveis e custos de estacionamento.
+        Transporte: Despesas associadas ao deslocamento, incluindo abastecimento de veículos, uso de transporte público, serviços de transporte por aplicativo, manutenção de automóveis e custos de estacionamento.
 
-Moradia: Gastos vinculados à residência, abrangendo pagamentos de aluguel ou financiamento imobiliário, contas de serviços públicos (energia elétrica, água, gás), serviços de comunicação (internet, telefone) e despesas com manutenção ou reparos domésticos.
+        Moradia: Gastos vinculados à residência, abrangendo pagamentos de aluguel ou financiamento imobiliário, contas de serviços públicos (energia elétrica, água, gás), serviços de comunicação (internet, telefone) e despesas com manutenção ou reparos domésticos.
 
-Saúde: Despesas destinadas ao bem-estar físico e mental, como planos de saúde, compra de medicamentos, consultas médicas, exames laboratoriais e terapias diversas.
+        Saúde: Despesas destinadas ao bem-estar físico e mental, como planos de saúde, compra de medicamentos, consultas médicas, exames laboratoriais e terapias diversas.
 
-Educação: Investimentos em formação e aprendizado, incluindo mensalidades escolares ou universitárias, cursos de aprimoramento, aquisição de materiais didáticos e livros.
+        Educação: Investimentos em formação e aprendizado, incluindo mensalidades escolares ou universitárias, cursos de aprimoramento, aquisição de materiais didáticos e livros.
 
-Lazer e Entretenimento: Gastos relacionados a atividades recreativas e culturais, como ingressos para cinema, teatro, shows, viagens, assinaturas de serviços de streaming e academias.
+        Lazer e Entretenimento: Gastos relacionados a atividades recreativas e culturais, como ingressos para cinema, teatro, shows, viagens, assinaturas de serviços de streaming e academias.
 
-Vestuário e Serviços de beleza: Compras de roupas, calçados, acessórios pessoais, serviços de spa, barbearias entre outros.
+        Vestuário e Serviços de beleza: Compras de roupas, calçados, acessórios pessoais, serviços de spa, barbearias entre outros.
 
-Despesas Financeiras: Custos associados a serviços financeiros, como juros, multas, tarifas bancárias e anuidades de cartões de crédito.
+        Despesas Financeiras: Custos associados a serviços financeiros, como juros, multas, tarifas bancárias e anuidades de cartões de crédito.
 
-Transferência Pessoal: Transações realizadas entre contas de pessoas físicas, geralmente envolvendo transferências entre familiares, amigos ou conhecidos para fins pessoais, como empréstimos informais, presentes ou compartilhamento de despesas.
+        Transferência Pessoal: Transações realizadas entre contas de pessoas físicas, geralmente envolvendo transferências entre familiares, amigos ou conhecidos para fins pessoais, como empréstimos informais, presentes ou compartilhamento de despesas.
 
-Presentes e Doações: Transações destinadas à aquisição de presentes para terceiros ou contribuições para instituições de caridade.
+        Presentes e Doações: Transações destinadas à aquisição de presentes para terceiros ou contribuições para instituições de caridade.
 
-Investimentos e Poupança: Aplicações financeiras visando retorno futuro, como depósitos em poupança, investimentos em ações, títulos ou aportes em previdência privada.
+        Investimentos e Poupança: Aplicações financeiras visando retorno futuro, como depósitos em poupança, investimentos em ações, títulos ou aportes em previdência privada.
 
-Despesas com Animais de Estimação: Gastos com cuidados de pets, incluindo alimentação, consultas veterinárias e produtos ou serviços relacionados.
+        Despesas com Animais de Estimação: Gastos com cuidados de pets, incluindo alimentação, consultas veterinárias e produtos ou serviços relacionados.
 
-Outras Despesas: Demais gastos que não se enquadram nas categorias anteriores, como pagamento de impostos, taxas diversas, serviços domésticos e assinaturas variadas."""])
+        Outras Despesas: Demais gastos que não se enquadram nas categorias anteriores, como pagamento de impostos, taxas diversas, serviços domésticos e assinaturas variadas."""])
         
         data: str = Field(description="Data da transferência")
         entrada_ou_saida: str = Field(description="Se a transferência foi de entrada ou saída")
